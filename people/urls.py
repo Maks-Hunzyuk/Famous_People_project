b235e3c3ec0b23d281c5c0e507924e8af7da769a
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2023-12-31 14:59:51 +0300

    31.12.23 Added dynamic url addresses

--- people/urls.py
+++ people/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, register_converter
 
-from people.views import index
+from people.views import index, categories, categories_by_slug, archive
+from people.converters import FoundDigitsYearConverter
 
 
 app_name = 'people'
 
 
+register_converter(FoundDigitsYearConverter, 'years4')
+
 urlpatterns = [
-    path('index/', index, name='index')
+    path('', index, name='index'),
+    path('categories/<int:category_id>/', categories, name='category'),
+    path('categories/<slug:category_slug>/', categories_by_slug, name='category_by_slug'),
+    path("^archive/<year4: year>/", archive, name='archive')
 ]
--- people/views.py
+++ people/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Страница приложения People")
+
+def categories(request, category_id):
+    return HttpResponse(f"<h1>Страница с категориями</h1><p>id: {category_id}")
+
+def categories_by_slug(request, category_slug):
+    return HttpResponse(f"<h1>Страница с категориями</h1><p>slug: {category_slug}")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}")
