7b6716193ce4b93fc4b40e226ab82c3bc564068b
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2024-01-02 10:52:07 +0300

    02.01.24 Added a slug field to the People model
    Added the slug field to the People model as well as displaying articles by slug

--- people/views.py
+++ people/views.py
@@ -1,59 +1,92 @@
 from django.http import HttpResponseNotFound, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from people.models import People
 
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти',]
-url_name = ['about', 'add_page', 'contact', 'login']
+menu = [
+    "О сайте",
+    "Добавить статью",
+    "Обратная связь",
+    "Войти",
+]
+url_name = ["about", "add_page", "contact", "login"]
 
 
 data_db = [
-    {'id': 1, 'title': 'Аднжелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Роббертс', 'content': 'Биография Джулии Роббертс', 'is_published': True}
+    {
+        "id": 1,
+        "title": "Аднжелина Джоли",
+        "content": "Биография Анджелины Джоли",
+        "is_published": True,
+    },
+    {
+        "id": 2,
+        "title": "Марго Робби",
+        "content": "Биография Марго Робби",
+        "is_published": False,
+    },
+    {
+        "id": 3,
+        "title": "Джулия Роббертс",
+        "content": "Биография Джулии Роббертс",
+        "is_published": True,
+    },
 ]
 
 categories_db = [
-    {'id': 1, 'name': 'Актрисы'},
-    {'id': 2, 'name': 'Актеры'},
-    {'id': 3, 'name': 'Спортсмены'},
-    {'id': 4, 'name': 'Спортсменки'},
+    {"id": 1, "name": "Актрисы"},
+    {"id": 2, "name": "Актеры"},
+    {"id": 3, "name": "Спортсмены"},
+    {"id": 4, "name": "Спортсменки"},
 ]
+
 
 def index(request):
     context = {
-               'menu': menu,
-               'title': 'Главная страница',
-               'posts': data_db,
-               'url_name': url_name,
-               'category_selected': 0
-               }
-    return render(request, 'people/index.html', context)
+        "menu": menu,
+        "title": "Главная страница",
+        "posts": data_db,
+        "url_name": url_name,
+        "category_selected": 0,
+    }
+    return render(request, "people/index.html", context)
+
 
 def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id {post_id}')
+    post = get_object_or_404(People, pk=post_id)
+
+    context = {"title": post.title, "menu": menu, "post": post, "category_selected": 1}
+    return render(request, "people/post.html", context)
+
 
 def show_category(request, category_id):
     context = {
-        'title': 'Отображние по категориям',
-        'menu': menu,
-        'posts': data_db,
-        'category_selected': category_id
+        "title": "Отображние по категориям",
+        "menu": menu,
+        "posts": data_db,
+        "category_selected": category_id,
     }
 
-    return render(request, 'people/index.html', context)
+    return render(request, "people/index.html", context)
+
 
 def add_page(request):
-    return HttpResponse('Добавление статьи')
+    return HttpResponse("Добавление статьи")
+
 
 def contact(request):
-    return HttpResponse('Обратная связь')
+    return HttpResponse("Обратная связь")
+
 
 def login(request):
-    return HttpResponse('Авторизация')
+    return HttpResponse("Авторизация")
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
+
 def about(request):
-    context = {'title': 'О сайте'}
-    return render(request, 'people/about.html', context)
+    context = {"title": "О сайте"}
+    return render(request, "people/about.html", context)
