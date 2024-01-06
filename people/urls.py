0007e50662f019401a35960dc10b07e2e9924276
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2024-01-06 12:01:32 +0300

    06.01.24 Transferred all controllers from functions to classes

--- people/urls.py
+++ people/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, register_converter
 
-from .views import index, about, show_post, add_page, contact, login, show_category, show_tag_post_list
+from .views import IndexTemplateView, about, ShowPostView, AddPageView, contact, login, PeopleCategoryView, TagPostListView
 from .converters import FoundDigitsYearConverter
 
 
@@ -10,12 +10,12 @@ app_name = 'people'
 register_converter(FoundDigitsYearConverter, 'year4')
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', IndexTemplateView.as_view(), name='home'),
     path('about/', about, name='about'),
-    path('post/<slug:post_slug>/', show_post, name='post'),
-    path('addpage/', add_page, name='add_page'),
+    path('post/<slug:post_slug>/', ShowPostView.as_view(), name='post'),
+    path('addpage/', AddPageView.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('category/<slug:category_slug>/', show_category, name='category'),
-    path('tag/<slug:tag_slug>/', show_tag_post_list, name='tag')
+    path('category/<slug:category_slug>/', PeopleCategoryView.as_view(), name='category'),
+    path('tag/<slug:tag_slug>/', TagPostListView.as_view(), name='tag')
 ]
--- people/views.py
+++ people/views.py
@@ -1,7 +1,11 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
+from django.views import View
+from django.views.generic import DetailView, ListView
 
-from .models import People, Categories, TagPost, UploadFile
+from .models import People, TagPost, UploadFile
 from .forms import AddPostForm, UploadFileForm
 
 
@@ -11,49 +15,6 @@ menu = [
     "Обратная связь",
     "Войти",
 ]
-
-
-def index(request):
-    posts = People.published.all().select_related("category")
-    context = {
-        "menu": menu,
-        "title": "Главная страница",
-        "posts": posts,
-        "category_selected": 0,
-    }
-    return render(request, "people/index.html", context)
-
-
-def show_post(request, post_slug):
-    post = get_object_or_404(People, slug=post_slug)
-
-    context = {"title": post.title, "menu": menu, "post": post, "category_selected": 1}
-    return render(request, "people/post.html", context)
-
-
-def show_category(request, category_slug):
-    category = get_object_or_404(Categories, slug=category_slug)
-    posts = People.published.filter(category_id=category.pk).select_related("category")
-    context = {
-        "title": f"Рубрика: {category.name}",
-        "menu": menu,
-        "posts": posts,
-        "category_selected": category.pk,
-    }
-
-    return render(request, "people/index.html", context)
-
-
-def add_page(request):
-    if request.method == "POST":
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect("people:home")
-    else:
-        form = AddPostForm()
-    context = {"menu": menu, "title": "Добавление статьи", "form": form}
-    return render(request, "people/addpage.html", context)
 
 
 def contact(request):
@@ -72,7 +33,7 @@ def about(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            item = UploadFile(file=form.changed_data['file_upload'])
+            item = UploadFile(file=form.changed_data["file_upload"])
             item.save()
     else:
         form = UploadFileForm()
@@ -80,16 +41,85 @@ def about(request):
     return render(request, "people/about.html", context)
 
 
-def show_tag_post_list(request, tag_slug):
-    tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=People.Status.PUBLISHED).select_related(
-        "category"
-    )
+class AddPageView(View):
+    def get(self, request):
+        form = AddPostForm()
+        context = {
+            "menu": menu,
+            "title": "Добавление статьи",
+            "form": form,
+        }
 
-    context = {
-        "title": f"Тег: #{tag.tag}",
-        "menu": menu,
-        "posts": posts,
-        "category_selected": None,
-    }
-    return render(request, "people/index.html", context)
+        return render(request, "people/addpage.html", context)
+
+    def post(self, request):
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("people:home")
+        context = {"menu": menu, "title": "Добавление статьи", "form": form}
+        return render(request, "people/addpage.html", context)
+
+
+class IndexTemplateView(ListView):
+    # model = People
+    template_name = "people/index.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return People.published.all().select_related("category")
+
+
+class PeopleCategoryView(ListView):
+    template_name = "people/index.html"
+    context_object_name = "posts"
+    allow_empty = False
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return People.published.filter(
+            category__slug=self.kwargs["category_slug"]
+        ).select_related("category")
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        item = context["posts"][0].category
+        context["title"] = "Категория - " + item.name
+        context["menu"] = menu
+        context["category_selected"] = item.pk
+        return context
+
+
+class TagPostListView(ListView):
+    template_name = "people/index.html"
+    context_object_name = "posts"
+    allow_empty = False
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        tag = TagPost.objects.get(slug=self.kwargs["tag_slug"])
+        context["title"] = "Тег:" + tag.tag
+        context["menu"] = menu
+        context["category_selected"] = None
+        return context
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return People.published.filter(
+            tags__slug=self.kwargs["tag_slug"]
+        ).select_related("category")
+
+
+class ShowPostView(DetailView):
+    model = People
+    template_name = "people/post.html"
+    slug_url_kwarg = "post_slug"
+    context_object_name = "post"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["title"] = context["post"].title
+        context["menu"] = menu
+        return context
+
+    def get_object(self, queryset: QuerySet[Any] | None = ...):
+        return get_object_or_404(
+            People.published, slug=self.kwargs[self.slug_url_kwarg]
+        )
